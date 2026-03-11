cd574b81c3ba1d707ec5e496944a774847e2f9ca
Kaique <kaiquemascarenhasdossantos@gmail.com>
2026-03-11 09:28:11 -0300

    adding Genero.cs

---
+++ src/Projeto.Api/Projeto.Api/Domain/Entities/Genero.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Projeto.Api.Domain.Entities;
+
+public class Genero
+{
+    public int GeneroId {get; set;}
+    public string Nome {get; set;}
+    public ICollection<Jogo> Jogo { get; set; }
+    
+}

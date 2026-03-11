25cb204dd6d1baa3fff94b0c6c0aa6be54ce06db
Felipe <felipeaugustol.contato@gmail.com>
2026-03-11 08:54:00 -0300

    creating usuario.cs

--- src/Projeto.Api/Projeto.Api/Domain/Entities/Usuario.cs
+++ src/Projeto.Api/Projeto.Api/Domain/Entities/Usuario.cs
@@ -1,6 +1,15 @@
-﻿namespace DefaultNamespace;
+﻿using System;
+using System.Collections.Generic;
+
+namespace Projeto.Api.Domain.Entities;
 
 public class Usuario
 {
+    public int UsuarioId { get; set; }
+    public int string Nome { get; set; }
+    public string Email { get; set; }
+    public string Pais { get; set; }
+    public DateTime DataCadastro { get; set; }
+    public ICollection<Biblioteca> Bibliotecas { get; set; }
     
 }

3be96cdd2e37e8404e210092bb0de6f8ded65095
Kaique <kaiquemascarenhasdossantos@gmail.com>
2026-03-11 09:31:54 -0300

    adding desenvolvedora

---
+++ src/Projeto.Api/Projeto.Api/Domain/Entities/Desenvolvedora.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Projeto.Api.Domain.Entities;
+
+public class Desenvolvedora
+{
+    public int DesenvolvedoraId { get; set; }
+    public string Nome { get; set; }
+    public string Pais { get; set; }
+    public DateTime AnoFundacao { get; set; }
+    public ICollection<Jogo> Jogos { get; set; }
+
+}

bcaca5f1f5fbcc443c81c8ce1711ac90bb82873a
Kaique <kaiquemascarenhasdossantos@gmail.com>
2026-03-11 09:18:47 -0300

    adding jogo.cs

---
+++ src/Projeto.Api/Projeto.Api/Domain/Entities/Jogo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Projeto.Api.Domain.Entities;
+
+public class Jogo
+{
+    public int JogoId { get; set; }
+    public string Titulo { get; set; }
+    public decimal Preco { get; set; }
+    public DateTime DataLancamento { get; set; }
+    public int GeneroId { get; set; }
+    public int DesenvolvedoraId { get; set; }
+    public int PublisherID {get; set;}
+    
+    public Genero Genero { get; set; }
+
+    public Desenvolvedora Desenvolvedora { get; set; }
+
+    public Publisher Publisher { get; set; }
+
+    public ICollection<Conquista> Conquistas { get; set; }
+
+    public ICollection<Biblioteca> Bibliotecas { get; set; }
+}

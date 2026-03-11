5ac966808006bdbcf61b47fa08ab112494d6e513
Kaique <kaiquemascarenhasdossantos@gmail.com>
2026-03-11 09:36:03 -0300

    adding Publisher.cs

---
+++ src/Projeto.Api/Projeto.Api/Domain/Entities/Publisher.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+namespace Projeto.Api.Domain.Entities;
+
+public class Publisher
+{
+    public int PublisherId { get; set; }
+    public string Nome { get; set; }
+    public string Pais { get; set; }
+    public ICollection<Jogo> Jogos { get; set; }
+
+}
